0475e08f1ca9f8e0b6f244b20aaf5736dc3bf2c4
Zachary Zulanas <zzulanas@gmail.com>
2020-01-18 15:42:14 -0800

    basic router and pages added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Charities from './pages/Charities';
+import Account from './pages/Account';
+import Login from './pages/Login';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import "shards-ui/dist/css/shards.min.css"
@@ -7,12 +11,33 @@ import "shards-ui/dist/css/shards.min.css"
 function App() {
   return (
     <BrowserRouter>
-      <Route exact={true} path='/' render={() => (
-            <div className="App">
-              <Home />
-            </div>
-          )}/>
-
+      <div>
+        <Route exact={true} path='/' render={() => (
+              <div className="App">
+                <Home />
+              </div>
+            )}/>
+        <Route exact={true} path='/signup' render={() => (
+              <div className="App">
+                <Signup />
+              </div>
+            )}/>
+        <Route exact={true} path='/charities' render={() => (
+              <div className="App">
+                <Charities />
+              </div>
+            )}/>
+        <Route exact={true} path='/account' render={() => (
+              <div className="App">
+                <Account />
+              </div>
+            )}/>
+        <Route exact={true} path='/login' render={() => (
+              <div className="App">
+                <Login />
+              </div>
+            )}/>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/pages/Charities.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Charities extends Component {
+    render() {
+        return (
+            <div>
+                Charities page
+            </div>
+        )
+    }
+}
