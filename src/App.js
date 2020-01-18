50bb7e1a166e582e1b332a8b1d64aece40d6070b
Zachary Zulanas <zzulanas@gmail.com>
2020-01-18 15:32:55 -0800

    router changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import React from 'react';
-import logo from './svg/coinz.svg';
-import Searchbar from './components/Searchbar.js';
+import Home from './pages/Home';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import "shards-ui/dist/css/shards.min.css"
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Searchbar/>
-        </header>
-      </div>
+      <Route exact={true} path='/' render={() => (
+            <div className="App">
+              <Home />
+            </div>
+          )}/>
+
     </BrowserRouter>
   );
 }
