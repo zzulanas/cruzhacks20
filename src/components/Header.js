99709fd7aa997d9f83ec899764b9808483a50707
Zachary Zulanas <zzulanas@gmail.com>
2020-01-18 17:53:37 -0800

    navbar wortking

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import Logo from '../components/Logo';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
-                <a className="nav-item" href="about">About</a>
-                <a className="nav-item" href="charities ">Charities</a>
-                <a className="nav-item" href="charities ">Account</a>
-                <a className="nav-item" href="charities ">Login</a>
-                <a className="nav-item" href="charities ">Signup</a>
+                <Logo/>
+                <div className="nav-items">
+                    <a className="nav-item" href="about">About</a>
+                    <a className="nav-item" href="charities ">Charities</a>
+                    <a className="nav-item" href="charities ">Account</a>
+                    <a className="nav-item" href="charities ">Login</a>
+                    <a className="nav-item" href="charities ">Signup</a>
+                </div>
             </div>
         )
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,6 +22,10 @@ export default class Home extends Component {
                         Learn React
                     </a>
                     </header>
+                    <div className="test-rectangle">
+
+                        test
+                    </div>
             </div>
         )
     }
