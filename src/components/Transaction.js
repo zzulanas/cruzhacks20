6e82a9c15991f69914024f68f1084fcad2cadea9
tianap <tianam.pereira@gmail.com>
2020-01-19 01:38:24 -0800

    meeeehhhh

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import {Container, Row, Col} from 'react-bootstrap';
 import {Card,CardHeader,CardTitle,CardImg,CardBody,CardFooter,Button} from "shards-react";
 
 export default class Transaction extends Component {
-    render() {
+    render(props) {
         return (
-            <Card style={{ maxWidth: "80%", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" , borderRadius: "10px", minWidth:"20%", margin:"10%", backgroundColor:""}}>
+            <Card style={{ maxWidth: "80%", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" , borderRadius: "10px", minWidth:"20%", margin:"10%", backgroundColor:"#FFC300"}}>
             <CardBody>
-              <CardTitle>Transaction ID</CardTitle>
-              <p>Lorem ipsum dolor sit amet.</p>
-              <Button>Read more &rarr;</Button>
+              <CardTitle>Transaction</CardTitle>
+              <Container>
+                <Row>
+                  <Col>
+                  {this.props.compName}
+                  <br/>
+                  </Col>
+                  <Col>
+                  {this.props.amount}
+                  </Col>
+                </Row>
+              </Container>
             </CardBody>
           </Card>
         )
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -3,13 +3,16 @@ import Transaction from '../components/Transaction';
 import {Container, Row, Col} from 'react-bootstrap';
 import Plaid from '../components/Plaid';
 
+
+
 export default class Account extends Component {
+    
     render() {
         return (
             <div>
                 <div id="left" style={{borderRight:"5px solid #48C9B0", marginTop:"7%"}}>
-                    <Transaction/>
-                    <Transaction/>
+                    <Transaction compName="Amazon" amount="$0.42"/>
+                    <Transaction compName="Target" amount="$0.96"/>
                 </div>
                 <div id="right">
                 <Transaction/>
