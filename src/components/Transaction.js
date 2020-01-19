61386fe37acd1de15f1327297f6fbd5aa34dc330
Zachary Zulanas <zzulanas@gmail.com>
2020-01-19 00:46:00 -0800

    plaid

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,9 +4,9 @@ import {Card,CardHeader,CardTitle,CardImg,CardBody,CardFooter,Button} from "shar
 export default class Transaction extends Component {
     render() {
         return (
-            <Card style={{ maxWidth: "30%", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" , borderRadius: "10px", minWidth:"20%"}}>
+            <Card style={{ maxWidth: "80%", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" , borderRadius: "10px", minWidth:"20%", margin:"10%", backgroundColor:""}}>
             <CardBody>
-              <CardTitle>Lorem Ipsum</CardTitle>
+              <CardTitle>Transaction ID</CardTitle>
               <p>Lorem ipsum dolor sit amet.</p>
               <Button>Read more &rarr;</Button>
             </CardBody>
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -7,10 +7,13 @@ export default class Account extends Component {
     render() {
         return (
             <div>
-                <div style={{borderRight:"5px solid #48C9B0"}}>
+                <div id="left" style={{borderRight:"5px solid #48C9B0", marginTop:"7%"}}>
+                    <Transaction/>
                     <Transaction/>
                 </div>
-                <div>
+                <div id="right">
+                <Transaction/>
+                <Transaction/>
                 <Plaid/>
                 </div>
             </div>
