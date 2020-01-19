f37f4cc6b9280be89467cb3e60701b2f59bf80b5
nikhildodd <44418217+nikhildodd@users.noreply.github.com>
2020-01-18 19:25:58 -0800

    password thing

--- src/pages/Password.js
+++ src/pages/Password.js
@@ -6,11 +6,19 @@ import { FormInput } from "shards-react";
 export default class Password extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            hidden: true
+        };
+        this.toggleShow = this.toggleShow.bind(this)
+    }
+    toggleSize(){
+        this.setState({hidden: !this.state.hidden});
     }
 	render(){
 		return(
 			<div>
 			  	<FormInput placeholder="password" />
+                  <button>Show / Hide</button>
 		  	</div>
 	    );
 	}
