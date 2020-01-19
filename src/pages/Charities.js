2dfe43c37f234a2ca8339a9c814034c5193ded2e
Zachary Zulanas <zzulanas@gmail.com>
2020-01-19 11:18:32 -0800

    change da world

--- src/pages/Charities.js
+++ src/pages/Charities.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import Searchbar from '../components/Searchbar';
+import {Container, Row, Col} from 'react-bootstrap';
 
 export default class Charities extends Component {
     render() {
         return (
             <div>
+                <Container>
+                    <Row>
+                        <Col>
+                        <img src={"https://external-preview.redd.it/3LTDD8Eh74rpV-kc8-Fz5ku8bIA1ocqD5i3qxlodP5c.png?width=640&crop=smart&format=pjpg&auto=webp&s=27dcd44a9836a544a1e45a9e5e852d16c45c157b"}/>
+                        </Col>
+                    </Row>
+                </Container>
 
     		</div>
            	
