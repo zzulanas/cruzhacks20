fa4c8e50f0b72323119ebd4ccbd37e78c39ba51b
nikhildodd <44418217+nikhildodd@users.noreply.github.com>
2020-01-18 17:51:24 -0800

    added a  basic login screen

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import Searchbar from '../components/Searchbar';
+import logo from '../svg/C4coin.svg'
+import Email from './Email';
+import Password from './Password';
+
+
 
 export default class Login extends Component {
     render() {
         return (
             <div>
-                Login page
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        email:
+                    </p>
+                    <Email/>
+                    <br></br>
+                    <p>
+                        password:
+                    </p>
+                    <Password/>
+                </header>
             </div>
         )
     }
