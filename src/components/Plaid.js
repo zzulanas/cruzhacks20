a061c052ad6fd99bd5528212a7fa40930ce13a65
Zachary Zulanas <zzulanas@gmail.com>
2020-01-18 23:38:51 -0800

    added plaid

---
+++ src/components/Plaid.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import PlaidLink from 'react-plaid-link';
+
+export default class Plaid extends Component {
+    handleOnSuccess(token, metadata) {
+        console.log(token)
+        console.log(metadata)
+    }
+    handleOnExit() {
+        console.log("oopsy woopsy!");
+    }
+    render() {
+    return (
+        <PlaidLink
+        clientName="Your app name"
+        env="sandbox"
+        product={["auth", "transactions"]}
+        publicKey="489aba9a49ef531ca8b95182f6290f"
+        onExit={this.handleOnExit}
+        onSuccess={this.handleOnSuccess}>
+        Open Link and connect your bank!
+        </PlaidLink>
+    )
+    }
+}
--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import Transaction from '../components/Transaction';
 import {Container, Row, Col} from 'react-bootstrap';
+import Plaid from '../components/Plaid';
 
 export default class Account extends Component {
     render() {
         return (
             <div>
-            <Container>
-                <Row>
-                    <Col xs={3} md={4} style={{borderRight:"5px solid #48C9B0"}}>
-                        <Transaction/>
-                    </Col>
-                </Row>
-            </Container>
+                <div style={{borderRight:"5px solid #48C9B0"}}>
+                    <Transaction/>
+                </div>
+                <div>
+                <Plaid/>
+                </div>
             </div>
         )
     }
