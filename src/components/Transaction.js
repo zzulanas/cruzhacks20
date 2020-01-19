0649eb45ac2e6ad57505882b89a348607bd0bbc2
tianap <tianam.pereira@gmail.com>
2020-01-19 02:32:15 -0800

    edited account page

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -7,15 +7,15 @@ export default class Transaction extends Component {
         return (
             <Card style={{ maxWidth: "80%", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" , borderRadius: "10px", minWidth:"20%", margin:"10%", backgroundColor:"#FFC300"}}>
             <CardBody>
-              <CardTitle>Transaction</CardTitle>
               <Container>
                 <Row>
                   <Col>
-                  {this.props.compName}
+                  {this.props.compName}<br/>
+                  <font size="2" color="595959">XXX-XXX-1234</font>
                   <br/>
                   </Col>
                   <Col>
-                  {this.props.amount}
+                  <font size="+4">{this.props.amount}</font>
                   </Col>
                 </Row>
               </Container>
