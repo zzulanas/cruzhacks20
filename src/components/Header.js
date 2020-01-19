6c190ffd2d368748e7514a84ab0686f02767624c
nikhildodd <44418217+nikhildodd@users.noreply.github.com>
2020-01-18 17:55:46 -0800

    typo corrected

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Logo from '../components/Logo';
+import Logo from '../components/logo';
 
 export default class Navbar extends Component {
     render() {
